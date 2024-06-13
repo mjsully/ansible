95ab6eed57e0c68ebb6bf8efb973adc945cee68b
Matt Sullivan <mattys940@gmail.com>
2024-06-13 09:34:55 +0100

    Commit Tailscale subnet router deployment

---
+++ playbooks/rollouts/tailscale/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.d/99-tailscale.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.d/99-tailscale.conf
+sysctl -p /etc/sysctl.d/99-tailscale.conf
